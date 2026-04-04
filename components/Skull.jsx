d634142f32280a86ba5933a3ab89b844abb47315
Namanshukla2 <namanshukla79019@gmail.com>
2026-04-04 22:16:09 +0530

    final svg

--- components/Skull.jsx
+++ components/Skull.jsx
@@ -1,14 +1,25 @@
 import Image from 'next/image';
 
-export default function Skull({ className = '', style = {}, width = 100, height = 100 }) {
+export default function Skull({ className = '', style = {}, width = 200, height = 200 }) {
   return (
-    <Image
-      src="/skull.svg"
-      alt="Skull"
-      width={width}
-      height={height}
+    <div
+      style={{
+        backgroundColor: 'white',
+        borderRadius: '50%',
+        padding: '20px',
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...style
+      }}
       className={className}
-      style={style}
-    />
+    >
+      <Image
+        src="/skull.svg"
+        alt="Skull"
+        width={width}
+        height={height}
+      />
+    </div>
   );
 }
